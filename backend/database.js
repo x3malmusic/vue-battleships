7555e6ba408d8185c797568a83705233c0c49fcc
x3malmusic <x3malmusic@gmail.com>
2021-03-10 22:24:38 +0200

    database reconnect

--- backend/database.js
+++ backend/database.js
@@ -1,12 +1,20 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  });
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+    console.log(`connected: ${conn.connection.host}`);
+  } catch (e) {
+    console.log(e);
 
-  console.log(`connected: ${conn.connection.host}`);
+    // try to reconnect to database
+    setTimeout(async () => {
+      await connectDB();
+    }, 10000);
+  }
 };
