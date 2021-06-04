b63a283c10a3f276b061b0bb3867b71d379830ce
x3malmusic <x3malmusic@gmail.com>
2021-06-04 13:03:52 +0300

    cors

--- backend/server.js
+++ backend/server.js
@@ -18,12 +18,7 @@ connectDB();
 
 app.use(express.json({ extended: true }));
 
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use('/api/auth', authRoutes);
 app.use(errorHandler);
