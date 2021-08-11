fc633180637b2447f6aae02e9f75549f18dcfdea
yevhen_bonchev <x3malmusic@gmail.com>
2021-08-11 12:24:30 +0300

    added new services to dbService

--- backend/controllers/upload.js
+++ backend/controllers/upload.js
@@ -1,10 +1,7 @@
 import sharp from "sharp";
-import Stream from "stream";
-import { Avatar } from "../database";
-import { User } from "../models/User";
 import { asyncHandler } from "../middlewares/async";
-import { avatarImgName } from "../constants";
-import { NO_FILE_TO_UPLOAD, UPLOAD_FAILED, USER_NOT_FOUND, FILE_NOT_FOUND } from "../helpers/errorTypes";
+import { NO_FILE_TO_UPLOAD } from "../helpers/errorTypes";
+import { uploadImg, getImg } from "../services/dbService";
 
 export const uploadAvatar = asyncHandler( async (req, res, next) => {
   if (!req.files) return next(NO_FILE_TO_UPLOAD);
@@ -13,33 +10,12 @@ export const uploadAvatar = asyncHandler( async (req, res, next) => {
   const { image } = req.files;
 
   const data = await sharp(image.data).resize({ width: 40, height: 40 }).png({ compressionLevel: 6 }).toBuffer();
-  const stream = new Stream.PassThrough()
-  stream.end(data)
-
-  const options = ({ filename: avatarImgName, contentType: 'image/png' });
-
-  Avatar.write(options, stream, async (err, file) => {
-    if (err) return next(UPLOAD_FAILED)
-
-    const user = await User.findOne({_id: userId})
-    if (!user) return next(USER_NOT_FOUND);
-
-    if (user.avatar) Avatar.unlink(user.avatar, () => {})
-
-    user.avatar = file._id;
-    await user.save();
-
-    res.json(file._id);
-  });
+  const imageId = await uploadImg(data, userId)
+  res.json(imageId)
 });
 
 export const getAvatar = asyncHandler(async (req, res, next) => {
   const { id } = req.params;
-
-  Avatar.findById({_id: id}, (err, file) => {
-    if (err || !file) return next(FILE_NOT_FOUND)
-
-    const stream = file.read();
-    stream.pipe(res)
-  })
+  const imgStream = await getImg(id);
+  imgStream.pipe(res)
 })
--- backend/services/dbService.js
+++ backend/services/dbService.js
@@ -1,4 +1,8 @@
 import { User } from "../models/User";
+import Stream from "stream";
+import { avatarImgName } from "../constants";
+import { Avatar } from "../database";
+import { FILE_NOT_FOUND, UPLOAD_FAILED, USER_NOT_FOUND } from "../helpers/errorTypes";
 
 export const getUserByName = (name) => {
   return User.findOne({ name })
@@ -12,4 +16,37 @@ export const createUser = async (name, password) => {
   const user = new User({ name, password });
   await user.save();
   return user
+}
+
+export const uploadImg = (buffer, userId) => {
+  const stream = new Stream.PassThrough()
+  stream.end(buffer)
+
+  const options = ({ filename: avatarImgName, contentType: 'image/png' });
+  return new Promise((resolve, reject) => {
+    Avatar.write(options, stream, async (err, file) => {
+      if (err) return reject(UPLOAD_FAILED)
+
+      const user = await getUserById(userId)
+      if (!user) return reject(USER_NOT_FOUND);
+
+      if (user.avatar) Avatar.unlink(user.avatar, () => {})
+
+      user.avatar = file._id;
+      await user.save();
+
+      resolve(file._id)
+    });
+  })
+}
+
+export const getImg = (id) => {
+  return new Promise((resolve, reject) => {
+    Avatar.findById({_id: id}, (err, file) => {
+      if (err || !file) return reject(FILE_NOT_FOUND)
+
+      const stream = file.read();
+      resolve(stream)
+    })
+  })
 }
