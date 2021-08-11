c39b0e5af290a22c1f2f05f9107064f9a16eee2e
yevhen_bonchev <x3malmusic@gmail.com>
2021-08-11 15:07:35 +0300

    add Game model

---
+++ backend/models/Game.js
@@ -0,0 +1,11 @@
+import mongoose, { Schema, model } from "mongoose";
+
+const gameSchema = new Schema({
+  shots: { type: Number, default: 0 },
+  miss: { type: Number, default: 0 },
+  win: { type: Boolean, default: false },
+  bullseye: { type: Boolean, default: false },
+  user: [{ type: mongoose.Types.ObjectId, ref: "User" }],
+});
+
+export const User = model("Game", gameSchema);
