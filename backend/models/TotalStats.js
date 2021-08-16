868a09c142fbc2a01c1266803b41e7328775b525
yevhen_bonchev <x3malmusic@gmail.com>
2021-08-16 12:21:53 +0300

    add TotalStats model

---
+++ backend/models/TotalStats.js
@@ -0,0 +1,14 @@
+import mongoose, { Schema, model } from "mongoose";
+
+const totalStatsSchema = new Schema({
+  shots: { type: Number, default: 0 },
+  hit: { type: Number, default: 0 },
+  miss: { type: Number, default: 0 },
+  wins: { type: Number, default: 0 },
+  losses: { type: Number, default: 0 },
+  bullseye: { type: Number, default: 0 },
+  winrate: { type: String, default: '100%' },
+  user: { type: mongoose.Types.ObjectId, ref: "User" },
+});
+
+export const TotalStats = model("TotalStats", totalStatsSchema);
