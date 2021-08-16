b54125270aafa997f84cbc5d78a81a19ce22c68f
yevhen_bonchev <x3malmusic@gmail.com>
2021-08-16 13:58:03 +0300

    change worker

--- backend/worker.js
+++ backend/worker.js
@@ -1,14 +1,16 @@
 import process from "process";
 import { DATABASE_ERROR } from "./helpers/errorTypes";
 import { connectDB } from "./database";
-import { updateGameResult } from "./services/dbService";
+import { updateGameResult, updateTotalStats } from "./services/dbService";
 
 process.on("message", async (data) => {
   try {
     await connectDB()
-    const result = await updateGameResult(data)
-    process.send(result)
+    const lastGame = await updateGameResult(data)
+    const totalStats = await updateTotalStats(data)
+    process.send({ lastGame, totalStats })
   } catch (e) {
+    console.log(e)
     process.send({ error: DATABASE_ERROR })
   }
 })
